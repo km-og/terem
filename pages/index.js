40259d061af54c7b4b7dcc989d319bb581c9564e
Inessa Kim <kim.inessa20@yandex.ru>
2023-05-26 18:00:44 +0300

    fix: исправлены замечания

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,23 @@
-import { blockBtnHide, blockBtnSwap } from "../utils/constants.js";
-import {
-  handlePositionChanges,
-  handleVisibilityChanges,
-} from "../utils/utils.js";
+const block = document.querySelector(".block");
+const blockTitle = block.querySelector(".block__with-title");
+const blockBtnHide = block.querySelector(".btn-warning");
+const midBlock = block.querySelector(".middle__block");
+const blockBtnSwap = block.querySelector(".btn-success");
 
 alert("Привет, мир");
 
+// изменение видимости блока
+function handleVisibilityChanges() {
+  blockTitle.classList.toggle("block_type_hidden");
+}
+
 // слушатель изменения видимости блока
 blockBtnHide.addEventListener("click", handleVisibilityChanges);
+
+// поменять блоки местами
+function handlePositionChanges() {
+  midBlock.classList.toggle("block__swap");
+}
 
 // слушатель изменения положения блоков
 blockBtnSwap.addEventListener("click", handlePositionChanges);
